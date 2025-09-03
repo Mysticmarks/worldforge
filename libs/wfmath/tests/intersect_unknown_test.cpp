f23100311dd5b94481c0e7bb8f924fb84c62d3af
Mysticmarks <35151841+Mysticmarks@users.noreply.github.com>
2025-09-03 16:08:20 -0700

    Add safe dispatcher for polygon intersections

--- libs/wfmath/tests/intersect_unknown_test.cpp
+++ libs/wfmath/tests/intersect_unknown_test.cpp
@@ -3,6 +3,8 @@
 #include "wfmath/point_funcs.h"
 #include "wfmath/axisbox.h"
 #include "wfmath/axisbox_funcs.h"
+#include "wfmath/polygon.h"
+#include "wfmath/polygon_intersect.h"
 #include <assert.h>
 
 using namespace WFMath;
@@ -18,11 +20,24 @@ int main()
     assert(Intersect(box, p, false));
     assert(Intersect(p, box, false));
 
+    // Polygon combinations are implemented only with the polygon first
+    Polygon<2> poly;
+    poly.addCorner(0, p);
+    poly.addCorner(0, Point<2>(1, 0));
+    poly.addCorner(0, Point<2>(1, -1));
+    poly.addCorner(0, Point<2>(0, -1));
+    poly.isValid();
+    assert(Intersect(poly, box, false));
+    assert(Intersect(box, poly, false));
+
     // Completely unknown combinations should be safely handled
     DummyA a;
     DummyB b;
     assert(!Intersect(a, b, false));
     assert(!Intersect(b, a, true));
     assert(!Intersect(a, a, false));
+    // Mixed known/unknown combinations
+    assert(!Intersect(a, box, false));
+    assert(!Intersect(box, a, true));
     return 0;
 }
