33729cb70fe4350c2e3970be05f8f95d717bae42
Mysticmarks <35151841+Mysticmarks@users.noreply.github.com>
2025-09-19 11:45:29 -0700

    Fix TimeDiff normalization for negative durations

--- libs/wfmath/tests/timestamp_test.cpp
+++ libs/wfmath/tests/timestamp_test.cpp
@@ -38,4 +38,25 @@ TEST_CASE("timestamp_test")
 
     tsb += tda;
     CHECK(tsa < tsb);
+
+    SECTION("Normalizes negative durations")
+    {
+        TimeDiff negative(-1500);
+        auto [sec, usec] = negative.full_time();
+        CHECK(sec == -2);
+        CHECK(usec == 500000);
+
+        TimeDiff delta = TimeDiff(500) - TimeDiff(1200);
+        auto [delta_sec, delta_usec] = delta.full_time();
+        CHECK(delta_sec == -1);
+        CHECK(delta_usec == 300000);
+
+        TimeStamp ts = TimeStamp::epochStart();
+        ts += TimeDiff(1000);
+        ts -= TimeDiff(2500);
+        auto diff = ts - TimeStamp::epochStart();
+        auto [stamp_sec, stamp_usec] = diff.full_time();
+        CHECK(stamp_sec == -2);
+        CHECK(stamp_usec == 500000);
+    }
 }
